18692c2206796db3f331eab79bea0fc1769f817e
Alexey <74977372+Zaratogr@users.noreply.github.com>
2025-03-01 17:58:55 +0300

    Подправлено формирование кода

--- task1/TB_data_acquire.v
+++ task1/TB_data_acquire.v
@@ -1,119 +1,119 @@
 `timescale 1 ns / 1 ns
 
 module TB_data_acquire();
+  
+  parameter PERIOD = 10;
+  reg clk;
+  
+  always begin
+    clk = 1'b0;
+    #(PERIOD/2)
+    clk = 1'b1;
+    #(PERIOD/2);
+  end
+  
+  wire        adc_data_req_o;
+  wire [11:0] data_o;
+  wire        data_rdy_o;
+  
+  reg         reset_n_i = 1;
+  reg         adc_data_rdy_i = 1;
+  reg [11:0]  adc_data_i = 0;
+  reg         syncro_i = 0;
+  
+  data_acquire
+  data_acquire_inst(
+    .clk_i(clk),
+    .reset_n_i(reset_n_i),
+      
+  //ADC interface
+    .adc_data_req_o(adc_data_req_o),
+    .adc_data_rdy_i(adc_data_rdy_i),
+    .adc_data_i(adc_data_i),
     
-    parameter PERIOD = 10;
-    reg clk;
+  //Module output interface
+    .syncro_i(syncro_i),
+    .data_o(data_o),
+    .data_rdy_o(data_rdy_o)
+  );
+  
+  always begin
+    #(PERIOD*4.3); 
+    reset_n_i <= 0;
+    #(PERIOD*0.2);
+    reset_n_i <= 1;
+    #(PERIOD*5.5);
+    syncro_i <= 1;
+    #(PERIOD*2);
+    syncro_i <= 0;
+    @(posedge adc_data_req_o);
+    #(PERIOD*5);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*15);
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'h014;   //20
+    #(PERIOD);
+    @(posedge adc_data_req_o);
+    #(PERIOD*1);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*1);
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'hFF1;   //-15
+    #(PERIOD);
+    @(posedge adc_data_req_o);
+    #(PERIOD*5);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*15);
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'h020;   //32
+    #(PERIOD);
+    @(posedge adc_data_req_o);
+    #(PERIOD*5);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*15);
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'h002;   //2
+    #(PERIOD);
+    @(posedge adc_data_req_o);
+    #(PERIOD*5);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*15);
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'hFDC;   //-36
+    #(PERIOD);
+    @(posedge adc_data_req_o);
+    #(PERIOD*5);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*15);
     
-    always begin
-        clk = 1'b0;
-        #(PERIOD/2)
-        clk = 1'b1;
-        #(PERIOD/2);
-    end
+//    #(PERIOD*4.3); 
+//    reset_n_i <= 0;
+//    #(PERIOD*0.2);
+//    reset_n_i <= 1;
+//    #(PERIOD*5.5);
     
-    wire        adc_data_req_o;
-    wire [11:0] data_o;
-    wire        data_rdy_o;
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'h034;   //52
+    #(PERIOD);
+    @(posedge adc_data_req_o);
+    #(PERIOD*5);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*15);
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'h005;   //5
+    #(PERIOD);
+    @(posedge adc_data_req_o);
+    #(PERIOD*5);
+    adc_data_rdy_i <= 0;
+    #(PERIOD*15);
+    adc_data_rdy_i <= 1;
+    adc_data_i <= 12'h002;   //2
+    #(PERIOD);
+    //20-15+32+2-36+52+5+2=62 (3E)
+    //62/8 = 7.75 (8)
     
-    reg         reset_n_i = 1;
-    reg         adc_data_rdy_i = 1;
-    reg [11:0]  adc_data_i = 0;
-    reg         syncro_i = 0;
-    
-    data_acquire
-    data_acquire_inst(
-        .clk_i(clk),
-        .reset_n_i(reset_n_i),
-        
-    //ADC interface
-        .adc_data_req_o(adc_data_req_o),
-        .adc_data_rdy_i(adc_data_rdy_i),
-        .adc_data_i(adc_data_i),
-        
-    //Module output interface
-        .syncro_i(syncro_i),
-        .data_o(data_o),
-        .data_rdy_o(data_rdy_o)
-    );
-    
-    always begin
-       #(PERIOD*4.3); 
-       reset_n_i <= 0;
-       #(PERIOD*0.2);
-       reset_n_i <= 1;
-       #(PERIOD*5.5);
-       syncro_i <= 1;
-       #(PERIOD*2);
-       syncro_i <= 0;
-       @(posedge adc_data_req_o);
-       #(PERIOD*5);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*15);
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'h014;   //20
-       #(PERIOD);
-       @(posedge adc_data_req_o);
-       #(PERIOD*1);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*1);
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'hFF1;   //-15
-       #(PERIOD);
-       @(posedge adc_data_req_o);
-       #(PERIOD*5);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*15);
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'h020;   //32
-       #(PERIOD);
-       @(posedge adc_data_req_o);
-       #(PERIOD*5);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*15);
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'h002;   //2
-       #(PERIOD);
-       @(posedge adc_data_req_o);
-       #(PERIOD*5);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*15);
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'hFDC;   //-36
-       #(PERIOD);
-       @(posedge adc_data_req_o);
-       #(PERIOD*5);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*15);
-       
-//       #(PERIOD*4.3); 
-//       reset_n_i <= 0;
-//       #(PERIOD*0.2);
-//       reset_n_i <= 1;
-//       #(PERIOD*5.5);
-       
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'h034;   //52
-       #(PERIOD);
-       @(posedge adc_data_req_o);
-       #(PERIOD*5);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*15);
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'h005;   //5
-       #(PERIOD);
-       @(posedge adc_data_req_o);
-       #(PERIOD*5);
-       adc_data_rdy_i <= 0;
-       #(PERIOD*15);
-       adc_data_rdy_i <= 1;
-       adc_data_i <= 12'h002;   //2
-       #(PERIOD);
-       //20-15+32+2-36+52+5+2=62 (3E)
-       //62/8 = 7.75 (8)
-       
-       @(posedge data_rdy_o);
-       #(PERIOD*10);
-    end
-    
-endmodule
+    @(posedge data_rdy_o);
+    #(PERIOD*10);
+  end
+  
+dmodule
--- task1/candidate_task1.v
+++ task1/candidate_task1.v
@@ -10,55 +10,54 @@
 //Код оформить на языке Verilog HDL без использования конструкций SystemVerilog
 
 module data_acquire(
-	input clk_i,
-	input reset_n_i,
-	
+  input clk_i,
+  input reset_n_i,
+  
 //ADC interface
-	output reg   adc_data_req_o = 0,
-	input	     adc_data_rdy_i,
-	input [11:0] adc_data_i,
-	
+  output reg   adc_data_req_o = 0,
+  input        adc_data_rdy_i,
+  input [11:0] adc_data_i,
+  
 //Module output interface
-	input              syncro_i,
-	output reg [11:0]  data_o = 0,
-	output reg         data_rdy_o = 1'b1
-	
+  input              syncro_i,
+  output reg [11:0]  data_o = 0,
+  output reg         data_rdy_o = 1'b1
+  
 
 );
 
 //signals
-	reg        adc_data_rdy_i_1d = 1;
-	reg        adc_data_rdy_i_2d = 1;
-	reg [11:0] adc_data_i_1d = 0;
-	reg [14:0] data_sum = 0;
-	reg        syncro_i_1d = 0;
-	reg        syncro_i_2d = 0;
-	reg        strobe_syncro_i_1d = 0;
-	reg        strobe_syncro_i_2d = 0;
-	reg        adc_data_req_o_tmp = 0;
-	reg [0:7]  adc_data_req_o_tmp_reg = 0;
-	//reg        stop;
-	reg        stop_1d = 0;
-	reg        stop_2d = 0;
-	reg        data_rdy_o_tmp = 1'b0;
-	reg [2:0]  cnt8 = 0;
-	reg [2:0]  cnt8_1d = 0;
-	
-	integer    index;
-	
-	
-    assign strobe_syncro_i   = syncro_i_1d       & ~syncro_i_2d;
-    assign strobe_data_rdy_i = adc_data_rdy_i_1d & ~adc_data_rdy_i_2d & data_rdy_o_tmp;  //блокируем строб приема данных с АЦП, если не ожидаем приема данных
-	//or or_req_o(cut_syncro_i_3d, cut_syncro_i_2d, cut_syncro_i_1d, adc_data_req_o_tmp);
-	assign stop        = (cnt8 == 3'b000)&(cnt8_1d == 3'b111);
-	assign block_req_o = (cnt8 == 3'b111);
-	assign next_req    = strobe_data_rdy_i & ~block_req_o; //формируем сигнал для запросов 2-8, block_req_o -- блокирует 9-й запрос
-	reg    next_req_1d = 0;
-	reg    next_req_2d = 0;
-	assign round    = data_sum[2] & (data_sum[3] | ~data_sum[2] | data_sum[1] | data_sum[0]); //round to the nearest even // okrugleniye do blizhayshego chetnogo
-	
-	
-always @(posedge clk_i) begin
+  reg        adc_data_rdy_i_1d = 1;
+  reg        adc_data_rdy_i_2d = 1;
+  reg [11:0] adc_data_i_1d = 0;
+  reg [14:0] data_sum = 0;
+  reg        syncro_i_1d = 0;
+  reg        syncro_i_2d = 0;
+  reg        strobe_syncro_i_1d = 0;
+  reg        strobe_syncro_i_2d = 0;
+  reg        adc_data_req_o_tmp = 0;
+  reg [0:7]  adc_data_req_o_tmp_reg = 0;
+  //reg        stop;
+  reg        stop_1d = 0;
+  reg        stop_2d = 0;
+  reg        data_rdy_o_tmp = 1'b0;
+  reg [2:0]  cnt8 = 0;
+  reg [2:0]  cnt8_1d = 0;
+  
+  integer    index;
+  
+  
+  assign strobe_syncro_i   = syncro_i_1d       & ~syncro_i_2d;
+  assign strobe_data_rdy_i = adc_data_rdy_i_1d & ~adc_data_rdy_i_2d & data_rdy_o_tmp;  //блокируем строб приема данных с АЦП, если не ожидаем приема данных
+  assign stop        = (cnt8 == 3'b000)&(cnt8_1d == 3'b111);
+  assign block_req_o = (cnt8 == 3'b111);
+  assign next_req    = strobe_data_rdy_i & ~block_req_o; //формируем сигнал для запросов 2-8, block_req_o -- блокирует 9-й запрос
+  reg    next_req_1d = 0;
+  reg    next_req_2d = 0;
+  assign round    = data_sum[2] & (data_sum[3] | ~data_sum[2] | data_sum[1] | data_sum[0]); //round to the nearest even // округление до ближайшего четного
+  
+  
+  always @(posedge clk_i) begin
     adc_data_rdy_i_1d <= adc_data_rdy_i;
     adc_data_rdy_i_2d <= adc_data_rdy_i_1d;
     adc_data_i_1d     <= adc_data_i;
@@ -72,9 +71,8 @@ always @(posedge clk_i) begin
     
     
     adc_data_req_o_tmp_reg[0] <= adc_data_req_o_tmp;
-    for (index = 1; index < 8; index = index + 1) 
-    begin
-    	adc_data_req_o_tmp_reg [index] <= adc_data_req_o_tmp_reg [index-1];  //задерживаем первый запрос
+    for (index = 1; index < 8; index = index + 1) begin
+      adc_data_req_o_tmp_reg [index] <= adc_data_req_o_tmp_reg [index-1];  //задерживаем первый запрос
     end
     
     next_req_1d <= next_req;
@@ -86,46 +84,32 @@ always @(posedge clk_i) begin
     stop_1d <= stop;
     stop_2d <= stop_1d;
     
-end
+  end
 
-always @(posedge clk_i or negedge reset_n_i) begin
-    if (reset_n_i==0 | stop_2d==1)
-        data_rdy_o_tmp <= 0;
-    else
-    begin
-        if (strobe_syncro_i==1)
-            data_rdy_o_tmp <= 1;
+  always @(posedge clk_i or negedge reset_n_i) begin
+    if (reset_n_i==0 | stop_2d==1)  data_rdy_o_tmp <= 0;
+    else begin
+      if (strobe_syncro_i==1)       data_rdy_o_tmp <= 1;
     end
     
-    if (reset_n_i==0 | stop_2d==1)
-        data_sum <= 0;
-    else
-    begin
-        if (strobe_data_rdy_i==1)
-            data_sum <= data_sum+{adc_data_i_1d[11],adc_data_i_1d[11],adc_data_i_1d[11],adc_data_i_1d}; //сумматор с накоплением
+    if (reset_n_i==0 | stop_2d==1)  data_sum <= 0;
+    else begin
+      if (strobe_data_rdy_i==1)     data_sum <= data_sum+{adc_data_i_1d[11],adc_data_i_1d[11],adc_data_i_1d[11],adc_data_i_1d}; //сумматор с накоплением
     end
     
-    if (reset_n_i==0)
-        cnt8 <= 0;
-    else
-    begin
-        if (strobe_data_rdy_i==1)
-           cnt8 <= cnt8+1;      //счетчик полученных с АЦП данных
+    if (reset_n_i==0)           cnt8 <= 0;
+    else begin
+      if (strobe_data_rdy_i==1) cnt8 <= cnt8+1;      //счетчик полученных с АЦП данных
     end
     
-    if (reset_n_i==0)
-        data_o <= 0;
-    else
-    begin
-        if (stop)
-           data_o <= data_sum[14:3]+round;  //формирование результата: сдвиг вправо на три бита и округление
+    if (reset_n_i==0)  data_o <= 0;
+    else begin
+      if (stop)        data_o <= data_sum[14:3]+round;  //формирование результата: сдвиг вправо на три бита и округление
     end
     
-    if (reset_n_i==0)
-        cnt8_1d <= 0;
-    else
-        cnt8_1d <= cnt8;
+    if (reset_n_i==0)  cnt8_1d <= 0;
+    else               cnt8_1d <= cnt8;
     
-end
+  end
 
 endmodule
